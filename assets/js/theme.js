ff6daf0cd8780e86cb8b54b5df936fa5396f1bd7
Luc Bertin <lucbertin@outlook.com>
2020-11-24 21:59:12 +0100

    test js buttons for code preview

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -135,15 +135,15 @@ $(function() {
   }
 });
 
-// toggling code preview
-    var state = false;
+// // toggling code preview
+//     var state = false;
 
-    var button = document.getElementByClassName("code_preview");
-    button.onclick = function() {
-      state = !state;
-      if (state) {
-        button.innerHTML = "ON";
-      } else {
-        button.innerHTML = "OFF";
-      }
-    }
+//     var button = document.getElementByClassName("code_preview");
+//     button.onclick = function() {
+//       state = !state;
+//       if (state) {
+//         button.innerHTML = "ON";
+//       } else {
+//         button.innerHTML = "OFF";
+//       }
+//     }
