ae01a98d5f469f3736de14173fe6e6823b8c2456
Luc Bertin <lucbertin@outlook.com>
2020-12-13 12:56:45 +0100

    solving issue setting to do not show code for buttons

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -144,17 +144,21 @@ function togglingButtonPreview(el, state1, state2){
 }
 
 // selecting code that are larger in height than 80 but smaller than 200 to create a Show code buttons
-let code_blocks_medium_height = $("div.language-python div").filter( function() {
-    return ($(this).height() >= 80 && $(this).height() <= 200)
-});
-$("<button class='code_preview disable_code_preview'>Do not show code</button>").insertBefore(code_blocks_medium_height)
+// let code_blocks_medium_height = $("div.language-python div").filter( function() {
+//     return ($(this).height() >= 80 && $(this).height() <= 200)
+// });
+// $("<button class='code_preview disable_code_preview'>Do not show code</button>").insertBefore(code_blocks_medium_height)
 
 // automatic hiding for elements with height greater than 200
 $('div.language-python div').each(function(){
-    if($(this).height()>=200){
+    if($(this).height() > 200){
       $(this).toggle();
-      $("<button class='code_preview'>Show code</button>").insertBefore(this)
-    };
+      $("<button class='code_preview'>Show code</button>").insertBefore(this);
+    } else if ($(this).height() >= 80 && $(this).height() <= 200) {
+      $("<button class='code_preview disable_code_preview'>Do not show code</button>").insertBefore(this);
+    } else {
+        // pass
+    }
  });
 
 // create toggling behavior on buttons click to hide next element
